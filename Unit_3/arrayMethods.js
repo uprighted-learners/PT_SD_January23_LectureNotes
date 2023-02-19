260ece1d325758eb8545775e0c597bc38e7d79a6
KateLockhart <katelockhart707@gmail.com>
2023-02-19 15:17:50 -0500

    Finished array method notes.

--- Unit_3/arrayMethods.js
+++ Unit_3/arrayMethods.js
@@ -89,3 +89,133 @@ function eatMango(fruit) {
 console.log("Filter with function ex: ", filteredFruitFunctionEx);
 
 // console.log(eatMango("Baseball")) // demo function
+
+//--------------------------------------------------------
+
+//? .forEach()
+/* 
+  Runs 3 arguments:
+    - The value
+    - The index
+    - The array object itself
+
+  - forEach() helps us iterate over an array performing a function once per element in the array via a callback function.
+  - Is not executed for empty elements
+  - Does not change the original array
+*/
+
+let newFoodList = ["apple", "pear", "mushrooms", "cheese", "spagett"];
+
+// for (let i = 0; i < newFoodList.length; i++) {
+//   console.log(newFoodList[i]);
+// }
+
+// arrow function with concise body
+// newFoodList.forEach((item) => console.log("ForEach Method: ", item));
+
+// arrow func with block body
+// newFoodList.forEach((item, potato) => {
+//   console.log("ForEach with Index: ", potato, "Value: ", item);
+//   console.log();
+// })
+
+// declarative function
+// newFoodList.forEach(function (item) {
+//   console.log("Declaration sample: ", item);
+// })
+
+// Build a function to console.log item
+// Separating function from forEach, works
+function displayFoodItem(foodThing) {
+  console.log(`Function outside of forEach: ${foodThing}.`);
+}
+// function call
+//displayFoodItem("potato")
+
+// Pass our forEach method the function
+newFoodList.forEach(displayFoodItem);
+
+//--------------------------------------------------------
+//? .map()
+/* 
+  - Creates a new array from calling a function for every array element
+  - Calls the function once per element in an array
+  - Does not execute the function for empty elements
+  - Does not change the original array
+*/
+
+// Populated by for loop
+let numArray = [];
+// Populate by using .map on numArray
+let fizzBuzzArray = [];
+
+for (let k = 0; k <= 30; k++) {
+  numArray.push(k);
+}
+// Checking if numArray was populated
+//console.log("NumArray:", numArray);
+
+// Simple ex first
+// numArray.map((x) => fizzBuzzArray.push(x));
+// numArray.map((x) => fizzBuzzArray.push(x + 5));
+
+// If % by 3 print fizz, if % by 5 print buzz, if % by 3&5 print # (push to fizzBuzzArray)
+numArray.map((x) => {
+  // Use a conditional to check division
+  if (x % 15 === 0) {
+    fizzBuzzArray.push(x);
+  } else if (x % 3 === 0) {
+    fizzBuzzArray.push("Fizz");
+  } else if (x % 5 === 0) {
+    fizzBuzzArray.push("Buzz");
+  }
+});
+
+// Check if fizzBuzzArray populated
+console.log("FizzBuzzArray: ", fizzBuzzArray);
+
+// ? --------- map vs forEach example --------------
+
+let mainArr = [1, 2, 3, 4];
+
+// .forEach does NOT return anything, performs an action on each item, does not return a value
+let forEachEx = mainArr.forEach((i) => i);
+console.log("ForEach Ex: ", forEachEx);
+
+// .map returns a new array, perform action & creates/returns new value
+// let mapEx = mainArr.map((i) => i);
+let mapEx = mainArr.map((i) => i + 3);
+console.log("Map Ex: ", mapEx);
+
+let secondMap = mapEx.map((i) => i + 5);
+console.log("SecondMap Ex: ", secondMap);
+console.log(mainArr);
+
+// -----------------------------------------------
+//? .find()
+/* 
+  - Executes a function for each array element and returns the value of the first element that passes a test.
+  - Returns undefined if no elements are found.
+  - Does not execute the function for empty elements.
+  - Does not change the original array.
+*/
+
+let tmnt = ["Mikey", "Donnie", "Leo", "Raph", "Splinter", "Shredder", "Baxter"];
+
+let character = "Leo";
+console.log(
+  "Find Ex 1: ",
+  tmnt.find((c) => c === character)
+);
+
+character = "April";
+console.log(
+  "Find Ex 2: ",
+  tmnt.find((c) => c === character)
+);
+
+character = "Splinter"; // find will return true for splinter versus the value splinter because we checked all items via including the index
+console.log(
+  "Find Ex 2: ",
+  tmnt.find((c, i) => console.log("Character: ", c === character, "Index: ", i))
+);
